412ffbdcff239c32806dc340b535ab1112571ef9
sunlinqing <linqing_sun@lovelawu.com>
2017-11-09 11:48:10 +0800

    docs(jobs-extend): 添加分页查询接口说明文档

--- jobs-extend/src/main/java/com/lawu/jobsextend/PageJob.java
+++ jobs-extend/src/main/java/com/lawu/jobsextend/PageJob.java
@@ -12,6 +12,8 @@ public interface PageJob<T> extends SimpleJob {
 
     /**
      * 查询数据分页
+     * 如查询出来的数据带有状态，而且该状态在处理完后需要做变更，则忽略currentPage，
+     * 每次查询数据时加载第一页即可，另外，为了使得旧数据可以优先处理，查询时建议按时间或自增id正序排列
      * @param currentPage 当前页面（从1开始）
      * @param pageSize 每页数量
      * @return
